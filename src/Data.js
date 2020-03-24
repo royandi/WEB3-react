26fcd77decac7ecfd79f8c14aa8254e523aa5c4a
gezaramdaciptaan <gezaramdaciptaan@gmail.com>
2020-03-24 14:30:15 +0700

    first commit

---
+++ src/Data.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import Card from "./Card";
+
+const Data = () =>{
+    return (
+      <div>
+        <Card name="Roy" job="Programmer"/>
+        <Card name="Nicolas" job="Seniman"/>
+        <Card name="Naibaho" job="Designer"/>
+      </div>
+    )
+  }
+  
+  export default Data;
